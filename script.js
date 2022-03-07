206f6668030ebdce22437887a2598f4ad7a58d44
“PrinceXxShadows” <princexxshadows@gmail.com>
2022-03-07 08:58:31 -0600

    updated with shorthand

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let randomNum = Math.floor((Math.random() * 5));
 randomNum ++ ;
 console.log(randomNum)
 
-if (randomNum === 1) {console.log(`Equal to 1`); } else if(randomNum === 2) {console.log(`Equal to 2 or 3`);} else if (randomNum === 3) {console.log(`Equal to 2 or 3`);} else if (randomNum === 4) {console.log(`Greater than or equal to 4`);} else if (randomNum === 5) {console.log(`Greater than or equal to 4`);}
+if (randomNum === 1) {console.log(`Equal to 1`); } else if
+(randomNum === 2 || randomNum === 3) {console.log(`Equal to 2 or 3`);} else if (randomNum >= 4) {console.log(`Greater than or equal to 4`);} 
 
 if (randomNum !== 3){console.log(`Not equal to 3`)}
 
